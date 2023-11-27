70bf6395f81bda9bd2c13a4da7093f4cb4af4689
brk-a <fnjakai@outlook.com>
2023-11-27 04:39:59 +0300

    depthFirst

--- 0x04-dsa/1-depthFirst.js
+++ 0x04-dsa/1-depthFirst.js
@@ -1,12 +1,27 @@
-import {Node} from './0-nodeClass'
-
-const nodeArray = ["a", "b", "c", "d", "e", "f"]
-for(i of nodeArray){
-    const i = new Node(i)
-    console.info(i)
+class Node{
+    constructor(val){
+        this.val = val
+        this.left = null
+        this.right = null
+    }
 }
 
+const a = new Node("a")
+const b = new Node("b")
+const c = new Node("c")
+const d = new Node("d")
+const e = new Node("e")
+const f = new Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
 const depthFirstIterative = root => {
+    if(root===null) return []
+
     const stack = [root]
     const result = []
 
@@ -14,12 +29,14 @@ const depthFirstIterative = root => {
         const current = stack.pop()
         result.push(current)
 
-        if(current.left) stack.push(current.left)
         if(current.right) stack.push(current.right)
+        if(current.left) stack.push(current.left)
     }
+
+    return result.map(obj => (obj.val))
 }
 
+const depthFirstRecursive = root => {}
 
-module.exports = {
-    depthFirstIterative,
-}
+console.info(depthFirstIterative("depth-first iterative", a))
+console.info(depthFirstRecursive("depth-first recursive", a))
